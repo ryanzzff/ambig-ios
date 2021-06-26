1cab2a7dae1bc9738e2014d4e4c010dbaf9013e1
ryanzzff <ryanzzff@gmail.com>
2021-06-26 23:08:50 +0800

    Add sample test case

---
+++ ambig-framework/ambig-framework/Hello World/HelloWorld.swift
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.swift
+//  ambig-framework
+//
+//  Created by Ryan Lee on 26/6/2021.
+//
+
+import Foundation
+
+public class HelloWorld {
+    public static func hello() -> String {
+        return "Hello World"
+    }
+}
